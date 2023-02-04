b4056cf44db5db600b8ac3682fe5afb0509c146d
Nicholas-David Labarre <ndlabarre@coveo.com>
2023-02-04 14:58:08 -0500

    Clean up spinning viz

--- frontend/src/SpinningViz.js
+++ frontend/src/SpinningViz.js
@@ -1,8 +1,8 @@
-import {Canvas, useFrame, useThree} from "@react-three/fiber";
-import {Center, OrbitControls, Text3D} from "@react-three/drei";
-import {useContext, useRef, useState} from "react";
-import {NowPlayingContext} from "./NowPlayingContext";
-import {useSpring, animated, config} from "@react-spring/three";
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { Center, OrbitControls, Text3D } from '@react-three/drei'
+import { forwardRef, useContext, useRef, useState } from 'react'
+import { NowPlayingContext } from './NowPlayingContext'
+import { useSpring, animated, config } from '@react-spring/three'
 
 export default function SpinningViz() {
   const song = useContext(NowPlayingContext)
@@ -12,86 +12,94 @@ export default function SpinningViz() {
       <directionalLight position={[10, 10, 10]} />
       <Scene song={song} />
       {/*<axesHelper scale={2} position={[0, 0, 0]} onUpdate={(self) => self.setColors('#ff2080', '#20ff80', '#2080ff')} />*/}
-      <OrbitControls enabled={false} enableZoom={false} enablePan={false} minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} />
+      <OrbitControls enabled={false} enableZoom={false} enablePan={false} minPolarAngle={Math.PI / 2}
+                     maxPolarAngle={Math.PI / 2} />
     </Canvas>
   )
 }
 
-function Scene({ margin = 0.5 , song }) {
+function Scene({ margin = 0.5, song }) {
   const { width, height } = useThree((state) => state.viewport)
   const position = {
     left: -width / 2 + margin,
     right: width / 2 - margin,
     top: height / 2 - margin,
-    bottom: -height / 2 + margin,
+    bottom: -height / 2 + margin
   }
 
-  const groupRef = useRef();
-  // TODO: If this causes performance issues, use useRef
-  // See: https://docs.pmnd.rs/react-three-fiber/advanced/pitfalls#%E2%9C%85-instead,-just-mutate,-use-deltas
-  const [normalVisible, setNormalVisible] = useState(true)
+  const groupRef = useRef()
+  const [artistRef, otherArtistRef, songRef, otherSongRef] = [useRef(), useRef(), useRef(), useRef()]
   useFrame((state, delta) => {
-    const angle = (state.clock.elapsedTime / 2) % (Math.PI * 2)
+    const angle = (groupRef.current.rotation.y + (delta / 1.25)) % (Math.PI * 2)
     groupRef.current.rotation.y = angle
-    // 270 to 90, when the text is vertical in the back, to when it's vertical in the front
-    setNormalVisible(angle < (Math.PI / 2) || angle > (Math.PI * 1.5))
-  })
 
-  const titleMesh = useRef()
-  const [active, setActive] = useState(false)
-  const { scale } = useSpring({
-    scale : active ? 0.75 : 1,
-    config: config.wobbly
+    // 270 to 90, when the text is vertical in the back, to when it's vertical in the front
+    const normalVisible = angle < (Math.PI / 2) || angle > (Math.PI * 1.5)
+    artistRef.current.visible = normalVisible
+    songRef.current.visible = normalVisible
+    otherArtistRef.current.visible = !normalVisible
+    otherSongRef.current.visible = !normalVisible
   })
 
   return (
     <group ref={groupRef}>
-      <Center bottom right position={[position.left, position.top, 0]} visible={normalVisible}>
-        <InfoText>
-          {song.artist}
-        </InfoText>
-      </Center>
-      <Center bottom left position={[position.left, position.top, 0]} rotation={[0, Math.PI, 0]} visible={!normalVisible}>
-        <InfoText>
-          {song.artist}
-        </InfoText>
-      </Center>
-      <Center top right position={[position.right, position.bottom, 0]} rotation={[0, Math.PI, 0]} visible={!normalVisible}>
-        <InfoText>
-          {song.album}
-        </InfoText>
-      </Center>
-      <Center top left position={[position.right, position.bottom, 0]} visible={normalVisible}>
-        <InfoText>
-          {song.album}
-        </InfoText>
-      </Center>
-      <animated.mesh scale={scale} onClick={() => setActive(!active)} ref={titleMesh}>
-      <Center rotation={[-0.30, -0.25, 0]}>
-          <Text3D
-            curveSegments={32}
-            bevelEnabled
-            bevelSize={0.04}
-            bevelThickness={0.1}
-            height={0.5}
-            lineHeight={0.5}
-            letterSpacing={-0.04}
-            size={1}
-            font="/Inter_Bold.json">
-            {song.title}
-            <meshNormalMaterial />
-          </Text3D>
-      </Center>
-    </animated.mesh>
+      <InfoText bottom right position={[position.left, position.top, 0]} ref={artistRef}>
+        {song.artist}
+      </InfoText>
+      <InfoText bottom left position={[position.left, position.top, 0]} rotation={[0, Math.PI, 0]} ref={otherArtistRef}>
+        {song.artist}
+      </InfoText>
+      <InfoText top left position={[position.right, position.bottom, 0]} ref={songRef}>
+        {song.album}
+      </InfoText>
+      <InfoText top right position={[position.right, position.bottom, 0]} rotation={[0, Math.PI, 0]} ref={otherSongRef}>
+        {song.album}
+      </InfoText>
+      <Title>
+        {song.title}
+      </Title>
     </group>
   )
 }
 
-function InfoText({children}) {
+const InfoText = forwardRef(({ children, ...props }, ref) => {
   return (
-    <Text3D letterSpacing={-0.01} size={0.5} font="/Inter_Bold.json">
-      {children}
-      <meshStandardMaterial color="white" />
-    </Text3D>
+    <Center {...props} ref={ref}>
+      <Text3D letterSpacing={-0.01} size={0.5} font='/Inter_Bold.json'>
+        {children}
+        <meshStandardMaterial color='white' />
+      </Text3D>
+    </Center>
+  )
+})
+
+function Title({ children }) {
+  const titleMesh = useRef()
+  const [active, setActive] = useState(false)
+  const { scale } = useSpring({
+    scale: active ? 0.1 : 1,
+    config: config.wobbly
+  })
+
+  return (
+    <animated.mesh scale={scale} onClick={() => {
+      setActive(!active)
+    }} ref={titleMesh}>
+      <Center rotation={[-0.30, -0.25, 0]}>
+        <Text3D
+          curveSegments={32}
+          bevelEnabled
+          bevelSize={0.04}
+          bevelThickness={0.1}
+          height={0.5}
+          lineHeight={0.5}
+          letterSpacing={-0.04}
+          size={1}
+          font='/Inter_Bold.json'>
+          {children}
+          <meshNormalMaterial />
+        </Text3D>
+      </Center>
+    </animated.mesh>
   )
 }
