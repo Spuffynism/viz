95a962245db0bd54ff125f23e2f0b95d22af3b7d
Nicholas-David Labarre <ndlabarre@coveo.com>
2023-06-26 19:18:39 -0400

    Make Goop text responsive

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,13 +2,14 @@ import Ascii from './scenes/ascii/Ascii'
 import Monolith from './scenes/monolith/Monolith'
 import Sparkling from './scenes/sparkling/Sparkling'
 import Goop from './scenes/goop/Goop'
+import Spinning from './scenes/spinning/Spinning'
 
 const config = {
-  showControls: true,
+  showControls: false,
   changeWithSong: false,
   scenes: [
     Ascii,
-    //Spinning,
+    Spinning,
     Monolith,
     //Dispersion,
     //Refraction,
--- frontend/src/scenes/goop/Goop.js
+++ frontend/src/scenes/goop/Goop.js
@@ -9,20 +9,41 @@ import {
 import { useControls } from 'leva'
 import { animated, config, useSpring } from '@react-spring/three'
 import * as THREE from 'three'
+import { Box, Flex, useReflow } from '@react-three/flex'
 
 const AnimatedText = animated(Text)
 const AnimatedPlane = animated(Plane)
+
+const normalizedExponentialDecay = (x) => {
+  return Math.E ** (-1 * (x / 150))
+}
+
+function TText({ textLength, children, ...props }) {
+  const reflow = useReflow()
+  const { viewport } = useThree()
+  console.log(textLength, 1/textLength)
+  return (
+    <AnimatedText
+      maxWidth={(viewport.width / 4) * 3}
+      fontSize={normalizedExponentialDecay(textLength)}
+      scale={1}
+      onSync={reflow}
+      {...props}>
+      {children}
+    </AnimatedText>
+  )
+}
 
 export default function Goop() {
   const song = useSong()
 
   const defaultTheme = {
     text: '#ffffff',
-    background: '#616161',
+    background: '#313131',
     dots: '#f25042'
   }
   const themes = [
-    defaultTheme,
+    //defaultTheme,
     {
       text: '#44348c',
       background: '#fef3e7',
@@ -30,6 +51,7 @@ export default function Goop() {
     },
     {
       ...defaultTheme,
+      background: '#313131',
       dots: '#569AFF'
     },
     {
@@ -42,18 +64,12 @@ export default function Goop() {
       dots: '#569AFF'
     }
   ]
-  const {theme} = useControls({theme: {
-    value: 0,
-    min: 0,
-    max: themes.length - 1,
-    step: 1
-  }})
 
   const [{ dots, background, text }] = useSpring(() => ({
     from: defaultTheme,
     to: async (next, cancel) => {
       const promises = themes.map(async (theme, i) => {
-        return new Promise((res) => setTimeout(() => res(theme), 30_000 * (i + 1)))
+        return new Promise((res) => setTimeout(() => res(theme), 60_000 * (i + 1)))
       })
 
       for (const promise of promises) {
@@ -81,19 +97,21 @@ export default function Goop() {
       <MovingBlob position={[4, 5, -2]} color={dots}/>
       <MovingBlob position={[6, -1, -2]} color={dots}/>
 
-      <Center>
-        <AnimatedText
-          color={text}
-          font='./6392477d42bd66577724519a_VCGooperSemiCondensed-Black.woff'
-          fontSize={1}
-        >
-          {song.title}
-          {'\n'}
-          {song.artist}
-          {'\n'}
-          {song.album}
-        </AnimatedText>
-      </Center>
+      <Flex justifyContent="center" alignItems="center" marginTop={1} marginRight={1}>
+        <Box centerAnchor>
+          <TText
+            color={text}
+            font='./6392477d42bd66577724519a_VCGooperSemiCondensed-Black.woff'
+            textLength={song.title.length + song.artist.length + song.album.length}
+          >
+            {song.title}
+            {'\n'}
+            {song.artist}
+            {'\n'}
+            {song.album}
+          </TText>
+        </Box>
+      </Flex>
     </Canvas>
   )
 }
