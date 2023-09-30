235fbeb322f39c4fdd938c12a095173463b1e5f3
Nicholas-David Labarre <ndlabarre@coveo.com>
2023-09-30 17:37:22 -0400

    rip out buggy eslint config, will re-add later

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
-
-const title = 'asiuhduashd';
 
 export default defineConfig(() => {
   return {
@@ -12,6 +9,6 @@ export default defineConfig(() => {
     build: {
       outDir: 'build',
     },
-    plugins: [react(), eslint()],
+    plugins: [react()],
   };
 });
