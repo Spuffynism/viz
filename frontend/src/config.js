82dba624427f5100b55eb30befeb0f7f6c0b60fa
Nico <nic.labarre@hotmail.com>
2024-08-02 19:08:20 -0400

    create new glitch scenes

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,16 +1,26 @@
 import Ascii from './scenes/ascii/Ascii'
 import Goop from './scenes/goop/Goop'
 import WireframeGoop from './scenes/wireframegoop/WireframeGoop'
+import FullWireframe from './scenes/fullwireframe/FullWireframe'
+import Sparkling from './scenes/sparkling/Sparkling'
+import Spinning from './scenes/spinning/Spinning'
+import Torus from './scenes/glitch/Torus'
+import Star from './scenes/glitch/Star'
 
 const config = {
   showControls: import.meta.env.DEV,
   changeWithSong: import.meta.env.PROD,
   scenes: [
+    Torus,
+    Star,
     Goop,
     WireframeGoop,
     Ascii,
+    FullWireframe,
+    Sparkling,
+    Spinning,
   ],
-  startScene: Goop.name,
+  startScene: Torus.name,
   strategy: 'Spotify'
 }
 
